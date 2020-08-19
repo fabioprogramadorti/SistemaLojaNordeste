12038b443790a2d92a88a37ac7e2a6dae2321db7
Fabio Santos <fabioprogramadorti@gmail.com>
2020-08-19 13:58:33 -0300

    modificaçao Projeto

--- serve/controllers/dashboard.controller.js
+++
@@ -1,9 +0,0 @@
-router.get("/historico", async (req, res) => {
-    try {
-     res.send(200);
-    } catch (error) {
-        return res.status(500).send(`ERRO:${error.message}`);
-    }
-});
-
-module.exports = router;
--- serve/routes/index.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const router = express.Router();
-
-router.use('/users', require('../controllers/user.controller'));
-router.use('/auth', require('../controllers/auth.controller'));
-
-module.exports = router;
